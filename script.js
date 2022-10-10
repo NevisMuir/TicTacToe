778ed6a2f59514c3f4163f8a5ef72e9b6db3b0a7
NevisMuir <rmuir310@gmail.com>
2022-10-10 17:34:21 -0400

    Complete game round

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const gameBoard = (() =>{
     let array=[0,1,2,3,4,5,6,7,8]
-    const erase = () =>{
+    //gameBoard.reset resets game array
+    const reset = () =>{
+        displayBoard.reset()
         for(let i=0;i<=8;i++){
             array.splice(i,1,i)
         }
     }
-    const write = (symbol,position)=>{
-        array.splice(position, 1, symbol)
+    const write = (position)=>{
+        array.splice(position, 1, gameFlow.symbol())
     }
-    const log = () =>{console.log(array)
-    }
+    
+    
     const checkWin = () =>{
         //all possible win case
         if(array[0]== array[1]&& array[1]==array[2]||
@@ -22,19 +24,21 @@ const gameBoard = (() =>{
             array[0]==array[4]&& array[4]==array[8]
             ){
                 console.log("winner!")
+                reset()
                 return
             }
         //tie case, array is full of strings and contains no numbers
         //but there is no winner     
         if(array.every(isNaN)){
             console.log('tie!')
+            reset()
+            return
         }
     }
 
     return{
-        erase,
+        reset,
         write,
-        log,
         checkWin
     }
 })()
@@ -43,6 +47,12 @@ const gameFlow = (() =>{
     let currentPlayer = 'X'
 
     const symbol = () => currentPlayer
+
+    const turn = () =>{
+        gameBoard.write(displayBoard.cellNum())
+        changePlayer()
+        gameBoard.checkWin()
+    }
 
     const changePlayer =() =>{
         if (currentPlayer == 'X'){
@@ -54,22 +64,41 @@ const gameFlow = (() =>{
         }
     }
     return{
-        changePlayer,
-        symbol
+        symbol,
+        turn
     }  
 })()
 
 const displayBoard = (()=>{
-    //not sure if this really requires being inside this module...
      const emptyCells=document.querySelectorAll('.unfilled')
+     let currentCell
+     const cellNum = ()=> currentCell
+    //prints x or o on cell, intiates turn
      emptyCells.forEach((cell)=>{
         cell.addEventListener('click',()=>{
             cell.textContent=gameFlow.symbol()
-            gameFlow.changePlayer()
             cell.classList.remove('unfilled')
-            cell.classList.add()
+            cell.classList.add('filled')
+            currentCell=cell.getAttribute("id")
+            gameFlow.turn()
         })
      })
+     //the above might make more sense in gameFlow? Since it's the action the player takes...
+     //idk, makes sense here to me
+     
+     //displayBoard.reset erases board
+     const reset = () =>{
+        document.querySelectorAll('.boardCell').forEach((cell)=>cell.textContent="")
+        document.querySelectorAll('.filled').forEach((cell)=>{
+            cell.classList.remove('filled')
+            cell.classList.add('unfilled')
+        })
+     }
+     return{
+        reset,
+        cellNum
+     }
+
    
 
 })()
